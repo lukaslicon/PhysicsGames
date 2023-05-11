47a21e2af8af887c9d652e0ddf365874cc455427
Lukas Licon <lukas@licons.com>
2023-05-11 10:18:53 -0700

    empty js files

---
+++ game.js
@@ -0,0 +1,10 @@
+const game = new Phaser.Game({
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    scene: [],
+    title: "Physics Based Games",
+});
