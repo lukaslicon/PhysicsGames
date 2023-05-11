1460f8df00d910428a1a0042da8eeffd479632df
Lukas Licon <lukas@licons.com>
2023-05-11 10:26:17 -0700

    all scenes established, no code

---
+++ intro.js
@@ -0,0 +1,19 @@
+class Intro extends Phaser.Scene {
+    constructor() {
+        super('intro')
+    }
+    preload(){
+
+    }
+    create() {
+        this.add.text(535,150, "Out with the Bugs!").setFontSize(80);
+        this.add.text(275, 400, "You are an exterminator and will need to get rid of all the bugs\n                   in this disgusting house...").setFontSize(35);
+        this.add.text(525, 825, "Exterminate 15 bugs to finish your job.").setFontSize(35);
+        this.add.text(775,950, "Click anywhere to begin.").setFontSize(20)
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('Scene1'));
+        });
+    }
+}
+
